c7609e1cd2980fe7363cd4391f0d9e00da1be536
Frank Zhou <frankzhou@Hotmail.ca>
2020-04-03 02:19:04 -0400

    Removed footer

--- pages/index.js
+++ pages/index.js
@@ -47,16 +47,6 @@ const Home = () => (
       </div>
     </main>
 
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
-
     <style jsx>{`
       .container {
         min-height: 100vh;
